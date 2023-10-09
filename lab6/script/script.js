3d095f8b27c88f100738927f4aad6e2893d99134
Partha Pratim Saha <sahapratim2@gmail.com>
2023-10-08 20:09:16 -0500

    lab6

--- lab6/script/script.js
+++ lab6/script/script.js
@@ -1,46 +1,51 @@
 console.log("---------------------------Answer 1--------------------------");
 let libraryBooks = [
-    { title: "The Road Ahead", author: "Bill Gates", libraryID: 1235 },
-    { title: "Walter Isaacson", author: "Steve Jobs", libraryID: 4268 },
-    { title: "The Road Ahead", author: "Bill Gates", libraryID: 4268 },
-    { title: "Mockingjay: The Final Book of The Hunger Games", author: "Suzanne Collins", libraryID: 3257 }
-   ];
-   function addBook(title, author, libraryID){
-    let book={title:title, author:author, libraryID:libraryID };
-    libraryBooks.push(book);
-    return book;
-  
-   }
-   let book=addBook("The Gift of Small Things"," Arundhuty Roy",6410);
-   console.log("Book : ");
-   console.log(book);
-   console.log("Library Books : ");
-   console.log( libraryBooks);
+  { title: "The Road Ahead", author: "Bill Gates", libraryID: 1235 },
+  { title: "Walter Isaacson", author: "Steve Jobs", libraryID: 4268 },
+  { title: "The Road Ahead", author: "Bill Gates", libraryID: 4268 },
+  { title: "Mockingjay: The Final Book of The Hunger Games", author: "Suzanne Collins", libraryID: 3257 }
+];
+function addBook(title, author, libraryID) {
+  let book = { title: title, author: author, libraryID: libraryID };
+  libraryBooks.push(book);
+  return book;
+}
 
-   console.log("---------------------------Answer 2--------------------------");
-   function getTitle(obj) {
-    return obj.title;
-  }
+let book = addBook("The Gift of Small Things", " Arundhuty Roy", 6410);
+console.log("Book : ");
+console.log(book);
+console.log("Library Books : ");
+console.log(libraryBooks);
 
-   function getTitles(){
-    return libraryBooks.map(getTitle).sort() ;
-   }
-   console.log("getTitles() : ");
-   console.log(getTitles());
-   console.log("---------------------------Answer 3--------------------------");
- function findBooks(title){
-    return libraryBooks.filter(x=> x.title.toUpperCase().includes(title.toUpperCase())).sort((a, b) => {
-        const authorA = a.author.toUpperCase();
-        const authorB = b.author.toUpperCase(); 
-        if (authorA < authorB) {
-          return -1;
-        }
-        if (authorA > authorB) {
-          return 1;
-        }
-        return 0;
-      });
-   }
-  console.log("findBooks('The') : ");
-  console.log(findBooks('The'));
+console.log("---------------------------Answer 2--------------------------");
+
+function getTitle(obj) {
+  return obj.title;
+}
+
+function getTitles() {
+  return libraryBooks.map(getTitle).sort();
+}
+
+console.log("getTitles() : ");
+console.log(getTitles());
+
+console.log("---------------------------Answer 3--------------------------");
+
+function findBooks(title) {
+  return libraryBooks.filter(x => x.title.toUpperCase().includes(title.toUpperCase())).sort((a, b) => {
+    const authorA = a.author.toUpperCase();
+    const authorB = b.author.toUpperCase();
+    if (authorA < authorB) {
+      return -1;
+    }
+    if (authorA > authorB) {
+      return 1;
+    }
+    return 0;
+  });
+}
+
+console.log("findBooks('The') : ");
+console.log(findBooks('The'));
 
--- lab6/script/scripttest.js
+++ lab6/script/scripttest.js
@@ -1,19 +1,18 @@
 describe("array", function(){
     it("addBook('The Gift of Small Things',' Arundhuty Roy',6410)", function(){
-        assert.deepEqual(addBook("The Gift of Small Things"," Arundhuty Roy",6410),{title: 'The Gift of Small Things', author: ' Arundhuty Roy', libraryID: 6410})
+        assert.deepEqual(addBook("The Gift of Small Things"," Arundhuty Roy",6410),{title: 'The Gift of Small Things', author: ' Arundhuty Roy', libraryID: 6410});
     })
 })
 
 describe("array", function(){
     it("getTitles()", function(){
-        assert.deepEqual(getTitles(),["Mockingjay: The Final Book of The Hunger Games", "The Gift of Small Things", "The Road Ahead', 'The Road Ahead', 'Walter Isaacson"])
-    })
+        chai.expect(getTitles()).to.include.deep.members(['Mockingjay: The Final Book of The Hunger Games', 'The Road Ahead', 'Walter Isaacson'])
+    });
 })
 
 describe("array", function(){
     it("findBooks('The')", function(){
-        assert.deepEqual(findBooks('The'),[
-            {title: 'The Gift of Small Things', author: ' Arundhuty Roy', libraryID: 6410},
+            chai.expect(findBooks('The')).to.include.deep.members([
             { title: "The Road Ahead", author: "Bill Gates", libraryID: 1235 },
             { title: "The Road Ahead", author: "Bill Gates", libraryID: 4268 },
             { title: "Mockingjay: The Final Book of The Hunger Games", author: "Suzanne Collins", libraryID: 3257 }
